f25148b83fb6579fb4c889b4b3f9e65ae32f6c4f
Fabien Mairesse <fabien.mairesse.externe@capgemini.com>
2020-03-26 09:13:44 +0100

    Improve styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-	{ path: '', component: HomeComponent },
+	{ path: 'home', component: HomeComponent },
 	{ path: 'posts', loadChildren: () => import('./pages/posts/posts.module').then(m => m.PostsModule) },
 	{ path: 'albums', loadChildren: () => import('./pages/albums/albums.module').then(m => m.AlbumsModule) },
-	{ path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) }
+	{ path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
+	{ path: '**', redirectTo: 'home' },
 ]
 
 @NgModule({
